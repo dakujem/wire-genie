a1bcda519a5d5b77b6458d9e13c8d688b978daa9
Andrej Rypo <xrypak@gmail.com>
2020-07-08 15:55:53 +0200

    Invoker and Constructor interfaces

---
+++ src/Invoker.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dakujem;
+
+/**
+ * A general interface for an object able to invoke callables.
+ * These implementations will usually provide arguments to the call.
+ *
+ * @author Andrej Rypák (dakujem) <xrypak@gmail.com>
+ */
+interface Invoker
+{
+    /**
+     * Invokes a callable.
+     * Returns the result of the call.
+     * Arguments provided to the call differ according to implementation.
+     *
+     * @param callable $target callable to be invoked
+     * @return mixed result of the $target callable invocation
+     */
+    public function invoke(callable $target);
+}
