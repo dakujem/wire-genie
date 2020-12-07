d147efd203d46bf6d0f5e1aad1ef5aaac1e64667
Andrej Rypo <xrypak@gmail.com>
2020-12-07 15:07:10 +0100

    cover cases not covered by v2 tests

--- tests/GenieCoreTest.php
+++ tests/GenieCoreTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Dakujem\Wire\Tests;
 
 use Dakujem\Sleeve;
+use Dakujem\Wire\Exceptions\Unresolvable;
+use Dakujem\Wire\Exceptions\UnresolvableCallArguments;
 use Dakujem\Wire\Genie;
 use PHPUnit\Framework\TestCase;
 
@@ -14,6 +16,7 @@ use PHPUnit\Framework\TestCase;
 class GenieCoreTest extends TestCase
 {
     use WithStuff;
+    use AssertsErrors;
 
     public function testGenieProvisionsCallArguments()
     {
@@ -69,5 +72,43 @@ class GenieCoreTest extends TestCase
         };
         $this->with(new Genie($c, $core), $contains);
         $this->with(Genie::employ($c, $core), $contains);
+    }
+
+    public function testInvocationTypeSwitch()
+    {
+        $g = new Genie($c = new Sleeve([
+            Constant::class => new Constant(0.0),
+            Coefficient::class => new Coefficient(1.0),
+            Offset::class => function ($c) {
+                return new Offset($c[Constant::class], $c[Coefficient::class]);
+            },
+        ]));
+
+        $o1 = $g(Offset::class);
+        $o2 = $g(function (Offset $offset) {
+            return $offset;
+        });
+
+        $this->assertSame($c[Offset::class], $o2);
+        $this->assertInstanceOf(Offset::class, $o1);
+        $this->assertNotSame($c[Offset::class], $o1); // must not be the same, one is created, other one is from the container
+        $self = $this;
+        $this->with($o1, function () use ($self, $c) {
+            // assert that the services contained inside the newly created Offset instance come from the container
+            $self->assertSame($c[Constant::class], $this->absolute);
+            $self->assertSame($c[Coefficient::class], $this->relative);
+        });
+    }
+
+    public function testUnresolvable()
+    {
+        $g = new Genie(new Sleeve([]), function () {
+            throw new UnresolvableCallArguments('foo');
+        });
+
+        $this->assertException(function () use ($g) {
+            $g(function () {
+            });
+        }, Unresolvable::class);
     }
 }
